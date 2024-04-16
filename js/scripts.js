9121ee146db57a2a8183baa4639224334cde0860
Murat Yasar <memluk@gmail.com>
2024-04-16 17:20:26 +0200

    Add dynamic content

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,9 @@ $(function(){
 (function (global){
    let rest = {};
    let homeHtml = "snippets/home-snippet.html";
+   let allCategoriesUrl = "https://github.com/murat-yasar/restaurant-page/blob/main/data/categories.json";
+   let categoriesTitleHtml = "snippets/categories-title-snippet.html";
+   let categoryHtml = "snippets/category-snippet.html";
 
    // Convinience function to insert innerHTML for 'select'
    let insertHtml = function (selector, html){
@@ -24,6 +27,13 @@ $(function(){
       insertHtml(selector, html);
    }
 
+   // Return substitute of '{{propName}}' with propValue in given 'string'
+   let insertProperty = function (string, propName, propValue) {
+      let propToReplace = "{{" + propName + "}}";
+      string = string.replace(new RegExp(propToReplace, "g"), propValue);
+      return string;
+   }
+
    // On page load
    document.addEventListener("DOMContentLoaded", function(event){
       // show home on first load
@@ -35,6 +45,28 @@ $(function(){
             .innerHTML = responseText;
          }, false);
    });
+
+   // Load the menu categories view
+   rest.loadMenuCategories = function() {
+      showLoading("#main-content");
+      $ajaxUtils.sendGetRequest( allCategoriesUrl, buildAndShowCategoriesHTML);
+   };
+
+   // Build HTML for the categories page based on the data from server
+   function buildAndShowCategoriesHTML(categories){
+      // Load title snippet of categories page
+      $ajaxUtils.sendGetRequest(
+         categoriesTitleHtml,
+         function(categoriesTitleHtml){
+            // Retrieve single category snippet
+            $ajaxUtils.sendGetRequest(
+               categoryHtml,
+               function(categoryHtml){
+                  let categoriesViewHtml = buildCategoriesViewHtml(categories, categoriesTitleHtml, categoryHtml);
+                  insertHtml("#main-content", categoriesViewHtml);
+               }, false);
+         }, false);
+   }
 
    global.$rest = rest;
 
